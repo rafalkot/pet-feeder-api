7791593311595d69022a3ff6f9c51da9ee15a996
Rafał Kot <rafal@rkot.pl>
2019-02-16 12:18:37 +0100

    Task update - spec tests & entity methods

--- tests/Spec/App/Domain/TaskSpec.php
+++ tests/Spec/App/Domain/TaskSpec.php
@@ -6,6 +6,7 @@ use App\Domain\PetId;
 use App\Domain\RecurrenceRule;
 use App\Domain\Task;
 use App\Domain\TaskId;
+use Assert\InvalidArgumentException;
 use PhpSpec\ObjectBehavior;
 
 class TaskSpec extends ObjectBehavior
@@ -37,5 +38,37 @@ class TaskSpec extends ObjectBehavior
         $this->petId()->shouldReturn($this->petId);
         $this->name()->shouldReturn($this->name);
         $this->recurrenceRule()->shouldReturn($this->recurrenceRule);
+    }
+
+    public function it_can_be_renamed()
+    {
+        $this->rename('New name');
+        $this->name()->shouldReturn('New name');
+    }
+
+    public function it_doesnt_allow_to_rename_with_empty_name()
+    {
+        $this->shouldThrow(InvalidArgumentException::class)
+            ->during('rename', ['']);
+    }
+
+    public function it_doesnt_allow_to_set_too_short_name()
+    {
+        $this->shouldThrow(InvalidArgumentException::class)
+            ->during('rename', ['Aa']);
+    }
+
+    public function it_doesnt_allow_to_set_too_long_name()
+    {
+        $this->shouldThrow(InvalidArgumentException::class)
+            ->during('rename', [str_repeat('A', 256)]);
+    }
+
+    public function it_allows_to_set_recurrence_rule()
+    {
+        $rule = RecurrenceRule::create(new \DateTimeImmutable(), 'FREQ=MONTHLY;');
+
+        $this->updateRecurrenceRule($rule);
+        $this->recurrenceRule()->shouldReturn($rule);
     }
 }
